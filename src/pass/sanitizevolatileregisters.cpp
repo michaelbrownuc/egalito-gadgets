679c9c9aa54e5beba3c952fd768040d7c766486d
Michael D. Brown <michael.brown@gtri.gatech.edu>
2021-05-24 14:35:20 -0400

    Udated notes on GCC compatibility.

--- src/pass/sanitizevolatileregisters.cpp
+++ src/pass/sanitizevolatileregisters.cpp
@@ -11,7 +11,13 @@
 
 /// SanitizeVolatileRegistersPass : Poisons compiler-placed return GPIs by exploiting calling conventions. Specifically, X86-64 calling conventions 
 /// mark RCX, and R8-11 as volatile registers not used for return values. We can sanitize these registers before returning from a function without 
-/// impacting normal program functionality. These iunstructions greatly impair gadget functionality, limiting gadget diversity and utility.
+/// impacting normal program functionality. These instructions greatly impair gadget functionality, limiting gadget diversity and utility.
+
+/// Compatibility note: GCC will violate the calling convention w.r.t. caller-saved registers at levels O2, O3, and Os. Specifically, it will not 
+/// insert save/restore instructions for caller-saved registers in the caller code if the called function does not use the register. Use of this 
+/// pass on GCC binaries produced at level O2, O3, and Os will likely result in unsound behavior in its current form.
+
+/// TODO: Improve this pass with intra- and inter-procedural data-flow analysis to achieve compatbility with GCC O2+ optimizations.
 
 void SanitizeVolatileRegistersPass::visit(Module *module) {
     recurse(module->getFunctionList());
